7bf521ef9d174f3352190d31f031cac3963547e2
Jim McGuffin <Jim@mcguffin.us>
2025-07-31 13:11:53 -0700

    Re-enable static export for static-only deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Add this line back.
+  // This tells Next.js to create a clean '/out' folder.
+  output: 'export',
+
   images: {
     unoptimized: true
   },
