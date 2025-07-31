695d03031b2a4c054ff1de3dce6535028e6611e9
Jim McGuffin <Jim@mcguffin.us>
2025-07-31 13:42:14 -0700

    Automated: Revert to static deployment to restore landing page

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Add this line back.
-  // This tells Next.js to create a clean '/out' folder.
   output: 'export',
-
   images: {
     unoptimized: true
   },
 };
 
-export default nextConfig; 
+export default nextConfig;
